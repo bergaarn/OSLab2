4e4d8aff7a95833b8768fc19e07a0d69f9281102
Joel Berg <joel.berg@hotmail.se>
2025-08-30 18:02:54 +0200

    Second Commit

--- test1.c
+++ test1.c
@@ -7,17 +7,21 @@
 
 int main(int argc, char **argv)
 {
-    struct link {
+    struct link 
+    {
         int x[SIZE][SIZE];
     };
-    struct link *start;
+
+    struct link* start;
     int iteration, i, j;
-    start = (struct link *) calloc(1, sizeof(struct link));
+    start = (struct link*) calloc(1, sizeof(struct link));
+    
     if (!start) 
     {
         printf("Fatal error: Can't allocate memory of %d x %d = %lu\n", SIZE, SIZE, (unsigned long)SIZE*SIZE);
         exit(-1);
     }
+    
     for (iteration = 0; iteration < ITERATIONS; iteration++) 
     {
         printf("test1, iteration: %d\n", iteration);
--- test2.c
+++ test2.c
@@ -8,18 +8,24 @@
 
 int main(int argc, char **argv)
 {
-    struct link {
+    struct link 
+    {
         int x[SIZE][SIZE];
     };
-    struct link *start;
+
+    struct link* start;
     int iteration;
-    FILE *f;
-    start = (struct link *) calloc(1, sizeof(struct link));
-    if (!start) {
+    FILE* f;
+    start = (struct link*) calloc(1, sizeof(struct link));
+    
+    if (!start) 
+    {
         printf("Fatal error: Can't allocate memory of %d x %d = %lu\n", SIZE, SIZE, (unsigned long)SIZE*SIZE);
         exit(-1);
     }
-    for (iteration = 0; iteration < ITERATIONS; iteration++) {
+    
+    for (iteration = 0; iteration < ITERATIONS; iteration++) 
+    {
         printf("test2, iteration: %d\n", iteration);
         f = fopen("/tmp/file1.txt", "w");
         fwrite(start->x, sizeof(start->x[0][0]), SIZE*SIZE, f);
